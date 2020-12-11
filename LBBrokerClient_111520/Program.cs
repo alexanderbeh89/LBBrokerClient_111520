63fd6b82f26de61034a9978936bec82faae02132
Alexander Beh <Alexander.Beh@wdc.com>
2020-12-11 16:19:11 +0800

    '121120'

---
+++ LBBrokerClient_111520/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroMQ;
+
+namespace LBBrokerClient_111520
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ZContext();
+            LBBroker_Client(context, 0);
+            Console.Read();
+        }
+
+        static void LBBroker_Client(ZContext context, int i)
+        {
+            // Create a socket
+            using (var client = new ZSocket(context, ZSocketType.REQ))
+            {
+                // Set a printable identity
+                client.IdentityString = "CLIENT" + i;
+
+                // Connect
+                client.Connect("tcp://127.0.0.1:6101");
+
+                using (var request = new ZMessage())
+                {
+                    request.Add(new ZFrame("Hellooo"));
+
+                    // Send request
+                    client.Send(request);
+                }
+
+                // Receive reply
+                using (ZMessage reply = client.ReceiveMessage())
+                {
+                    Console.WriteLine("CLIENT{0}: {1}", i, reply[0].ReadString());
+                }
+            }
+        }
+    }
+}
